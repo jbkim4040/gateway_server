d23358df25db209b6b5a5addf8773229ff5d2be5
jbkim4040 <jbkim404037@gmail.com>
2023-08-23 18:44:33 +0900

    게이트웨이 서비스 시작

---
+++ src/test/java/com/common/jbgw/JbGwApplicationTests.java
@@ -0,0 +1,13 @@
+package com.common.jbgw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JbGwApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
